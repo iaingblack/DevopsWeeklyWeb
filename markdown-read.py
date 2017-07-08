fb2bd032769e67a122733f8926733b7bc3c63305
Iain Black <iaingblack@gmail.com>
2017-07-08 12:00:02 +0100

    Broke down into sections

--- markdown-read.py
+++ markdown-read.py
@@ -1,16 +1,36 @@
-import io
+from pprint import pprint
 import md2py
 
 # Get content of the markdown file
 markdown = open("exampleissue.md", "r")
 content = markdown.read()
-
 toc = md2py.md2py(content)
-for item in toc:
-    print(item)
 
+#Empty list for now
+sections = []
 
-#>>> toc.h1
-#Chikin Tales
-#>>> str(toc.h1)
-#'Chikin Tales'
+# Print each heading
+for heading in toc:
+    # Some things have a newlines as well, split those too
+    individualLines = str(heading).splitlines()
+    for line in individualLines:
+        print(line)
+        print('             ')
+        if "title: Issue" in str(line):
+            title = line
+            print('  TITLE   - ' + str(title))
+        elif len(str(line)) < 20:
+            if "layout: post" not in line:
+                sections.append(line)
+        else:
+            print('Section seems to be a line of text')
+
+print('')
+print('===================================================')
+print('OVERVIEW BELOW')
+print('===================================================')
+print('  TITLE   - ' + str(title))
+for section in sections:
+    print('  SECTION - ' + str(section))
+#Prints the contents read in
+#pprint(vars(toc))
